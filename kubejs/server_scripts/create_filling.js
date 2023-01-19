e580fe2779f5b26b4eaab395d74ea58d1c49b5a7
Luciano Wayand de Abreu <36968613+lucianowayand@users.noreply.github.com>
2023-01-19 19:32:36 -0300

    fix: zinc ammount wrong

--- kubejs/server_scripts/create_filling.js
+++ kubejs/server_scripts/create_filling.js
@@ -1,6 +1,6 @@
 onEvent('recipes', (event) => {
     event.recipes.createFilling('kubejs:galvanized_steel', [
 		'tfc:metal/ingot/wrought_iron',
-		Fluid.of('tfc:metal/zinc', 250)
+		Fluid.of('tfc:metal/zinc', 25)
 	])
 })
